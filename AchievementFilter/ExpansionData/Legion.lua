25ab966d6271bb875c608251f0aba12638f3ea8b
Krowi <frederikvandevelde@outlook.com>
2020-11-17 13:41:12 +0100

    Legion raids added

--- AchievementFilter/ExpansionData/Legion.lua
+++ AchievementFilter/ExpansionData/Legion.lua
@@ -4,6 +4,7 @@ tinsert(KrowiAF.Data, Xpack_Legion);
 
 -- Raids
 Xpack_Legion_Raids = Xpack_Legion:AddChild(KrowiAF.AchievementCategory:New(AF_CATEGORY_RAIDS));
+Xpack_Legion_Raids:AddAchievementIDs({11180, 11763, 11987}); -- Glory of the ...
 
 -- 7.0
 Xpack_Legion_Raid_EN = Xpack_Legion_Raids:AddChild(KrowiAF.AchievementCategory:New(AF_CATEGORY_XPACK_LEGION_RAID_EN));
@@ -30,16 +31,16 @@ Xpack_Legion_Raid_ToV:AddAchievement(11580, KrowiAF.AchievementType.FoS, false);
 
 -- -- 7.2
 Xpack_Legion_Raid_ToS = Xpack_Legion_Raids:AddChild(KrowiAF.AchievementCategory:New(AF_CATEGORY_XPACK_LEGION_RAID_TOS));
--- Xpack_Legion_Raid_ToS:AddAchievementIDs({}); -- Glory of the Tomb Raider
--- Xpack_Legion_Raid_ToS:AddAchievementIDs({}); -- Defeat X on Mythic Difficulty
--- Xpack_Legion_Raid_ToS:AddAchievementIDs({}); -- Defeat the bosses in X
--- Xpack_Legion_Raid_ToS:AddAchievement(, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
--- Xpack_Legion_Raid_ToS:AddAchievement(, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
+Xpack_Legion_Raid_ToS:AddAchievementIDs({11724, 11699, 11676, 11696, 11773, 11674, 11675, 11683, 11770}); -- Glory of the Tomb Raider
+Xpack_Legion_Raid_ToS:AddAchievementIDs({11767, 11775, 11776, 11774, 11777, 11778, 11779, 11780, 11781}); -- Defeat X on Mythic Difficulty
+Xpack_Legion_Raid_ToS:AddAchievementIDs({11787, 11788, 11789, 11790}); -- Defeat the bosses in X
+Xpack_Legion_Raid_ToS:AddAchievement(11874, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
+Xpack_Legion_Raid_ToS:AddAchievement(11875, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
 -- -- 7.3
 Xpack_Legion_Raid_Antorus = Xpack_Legion_Raids:AddChild(KrowiAF.AchievementCategory:New(AF_CATEGORY_XPACK_LEGION_RAID_ANTORUS));
--- Xpack_Legion_Raid_Antorus:AddAchievementIDs({}); -- Glory of the Argus Raider
--- Xpack_Legion_Raid_Antorus:AddAchievementIDs({}); -- Defeat X on Mythic Difficulty
--- Xpack_Legion_Raid_Antorus:AddAchievementIDs({}); -- Defeat the bosses in X
--- Xpack_Legion_Raid_Antorus:AddAchievement(, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
--- Xpack_Legion_Raid_Antorus:AddAchievement(, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
+Xpack_Legion_Raid_Antorus:AddAchievementIDs({11930, 12065, 12129, 11928, 12067, 11949, 12030, 11948, 12046, 11915, 12257}); -- Glory of the Argus Raider
+Xpack_Legion_Raid_Antorus:AddAchievementIDs({11992, 11993, 11994, 11995, 11996, 11997, 11998, 11999, 12000, 12001, 12002}); -- Defeat X on Mythic Difficulty
+Xpack_Legion_Raid_Antorus:AddAchievementIDs({11988, 11990, 11989, 11991}); -- Defeat the bosses in X
+Xpack_Legion_Raid_Antorus:AddAchievement(12110, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
+Xpack_Legion_Raid_Antorus:AddAchievement(12111, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)

d4aa135673fd64e29df4fedf6ac1cc60cd214344
Krowi <frederikvandevelde@outlook.com>
2020-11-19 16:59:56 +0100

    Legion dungeons added and reordered raid achievments

--- Krowi_AchievementFilter/ExpansionData/Battle for Azeroth.lua
+++ Krowi_AchievementFilter/ExpansionData/Battle for Azeroth.lua
@@ -11,8 +11,8 @@ tinsert(KrowiAF.Data, expansion);
         -- 8.0
             raid = raids:AddChild(KrowiAF.AchievementCategory:NewEJ(1031)); -- Uldir
             raid:AddAchievementIDs({12937, 12938, 12828, 12823, 12772, 12830, 12836, 12551}); -- Glory of the Uldir Raider
-            raid:AddAchievementIDs({12524, 12526, 12527, 12530, 12529, 12531, 12532, 12533}); -- Defeat X on Mythic Difficulty
             raid:AddAchievementIDs({12521, 12522, 12523}); -- Defeat the bosses in X
+            raid:AddAchievementIDs({12524, 12526, 12527, 12530, 12529, 12531, 12532, 12533}); -- Defeat X on Mythic Difficulty
             raid:AddAchievement(12536, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
             raid:AddAchievement(12535, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
@@ -25,6 +25,11 @@ tinsert(KrowiAF.Data, expansion);
                 raid:AddAchievementIDs({13383, 13431}); -- Glory of the Dazar'alor Raider
             end
             raid:AddAchievementIDs({13345, 13325, 13425, 13401, 13430, 13410}); -- Glory of the Dazar'alor Raider
+            if KrowiAF.IsAlliance then
+                raid:AddAchievementIDs({13286, 13287, 13288}); -- Defeat the bosses in X
+            elseif KrowiAF.IsHorde then
+                raid:AddAchievementIDs({13289, 13290, 13291}); -- Defeat the bosses in X
+            end
             raid:AddAchievementIDs({13292}); -- Defeat X on Mythic Difficulty
             if KrowiAF.IsAlliance then
                 raid:AddAchievementIDs({13298, 13293}); -- Defeat X on Mythic Difficulty
@@ -32,34 +37,29 @@ tinsert(KrowiAF.Data, expansion);
                 raid:AddAchievementIDs({13293, 13295}); -- Defeat X on Mythic Difficulty
             end
             raid:AddAchievementIDs({13299, 13300, 13311, 13312, 13313, 13314}); -- Defeat X on Mythic Difficulty
-            if KrowiAF.IsAlliance then
-                raid:AddAchievementIDs({13286, 13287, 13288}); -- Defeat the bosses in X
-            elseif KrowiAF.IsHorde then
-                raid:AddAchievementIDs({13289, 13290, 13291}); -- Defeat the bosses in X
-            end
             raid:AddAchievement(13322, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
             raid:AddAchievement(13323, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
             raid = raids:AddChild(KrowiAF.AchievementCategory:NewEJ(1177)); -- Crucible of Storms
             raid:AddAchievementIDs({13501, 13506}); -- No Glory available
-            raid:AddAchievementIDs({13416, 13417}); -- Defeat X on Mythic Difficulty
             raid:AddAchievementIDs({13414, 13415}); -- Defeat the bosses in X
+            raid:AddAchievementIDs({13416, 13417}); -- Defeat X on Mythic Difficulty
             raid:AddAchievement(13418, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
             raid:AddAchievement(13419, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
         -- 8.2
             raid = raids:AddChild(KrowiAF.AchievementCategory:NewEJ(1179)); -- Eternal Palace
             raid:AddAchievementIDs({13684, 13628, 13767, 13629, 13724, 13633, 13716, 13768}); -- Glory of the Eternal Raider
-            raid:AddAchievementIDs({13726, 13728, 13727, 13729, 13730, 13731, 13732, 13733}); -- Defeat X on Mythic Difficulty
             raid:AddAchievementIDs({13718, 13719, 13725}); -- Defeat the bosses in X
+            raid:AddAchievementIDs({13726, 13728, 13727, 13729, 13730, 13731, 13732, 13733}); -- Defeat X on Mythic Difficulty
             raid:AddAchievement(13784, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
             raid:AddAchievement(13785, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
         -- 8.3
             raid = raids:AddChild(KrowiAF.AchievementCategory:NewEJ(1180)); -- Ny'alotha, the Waking City
             raid:AddAchievementIDs({14019, 14008, 14037, 14024, 14139, 14023, 13999, 13990, 14026, 14038, 14147, 14148}); -- Glory of the Ny'alotha Raider
+            -- raid:AddAchievementIDs({}); -- Defeat the bosses in X
             raid:AddAchievementIDs({14041, 14043, 14044, 14045, 14050, 14046, 14051, 14048, 14049, 14052, 14054, 14055}); -- Defeat X on Mythic Difficulty
-            raid:AddAchievementIDs({}); -- Defeat the bosses in X
             raid:AddAchievement(14068, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
             raid:AddAchievement(14069, KrowiAF.AchievementType.FoS, false);  -- Feats of Strength (Unobtainable)
 
